c7ba3d87080275d37ed7aa8b6df9a47cf5ca95c0
jcbcw <diannasinclar@aol.com>
2017-10-03 12:29:40 -0400

    refactor clock preparing for typescript integration

--- js/ch01_start.js
+++ js/ch01_start.js
@@ -1,10 +1,39 @@
 function onReady () {
-    console.log('Hello Chapter1');
+    setInterval(updateTime, 1000);
+}
 
-    var date = new Date();
-    document.getElementById("clock").innerHTML = date.getHours();
+function displayClock(clock, time){
+    
+    clock.innerHTML = updateTime(); 
+    
+}
+
+function updateTime(){
+    
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    
+    return getCurrentTime(hours, minutes, seconds);
+}
+
+function getCurrentTime (hours, minutes, seconds) {
+    
+    hours   = hours   < 10? `0${hours}`  : hours; 
+    minutes = minutes < 10? `0${minutes}`: minutes; 
+    seconds = seconds < 10? `0${seconds}`: seconds; 
+    
+    return displayClock(`${hours}:${minutes}:${seconds}`);   
+}
+
+function displayClock(time) {
+
+    let mainClock = document.getElementById("clock");
+    mainClock.innerHTML = time;
 
 }
+
 window.onload = onReady;
 
 //follow me at https://twitter.com/02geek
